ac85699ed0449a55f2101315e5829008bdbd15af
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-24 14:39:19 -0700

    add workflow

---
+++ .tools/create_active_json.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env -S uv run --script
+# /// script
+# dependencies = [
+#   "toml",
+#   "tzfpy[tzdata]",
+#   "whenever",
+# ]
+# ///
+
+import json
+import os
+import sys
+import toml
+import tzfpy
+import whenever
+
+cons = []
+
+for fn in os.listdir("."):
+    id, ext = os.path.splitext(fn)
+    if ext != ".toml":
+        continue
+    with open(fn) as f:
+        con = toml.load(f)
+    if "lat_lng" in con:
+        (lat, lng) = con["lat_lng"]
+        con["timezone"] = tzfpy.get_tz(lng, lat)
+    cons.append({"id": id, **con})
+
+now = whenever.Instant.now()
+
+
+def pred(con):
+    end_date = (
+        whenever.Date.parse_common_iso(con["end_date"])
+        .add(days=1)
+        .at(whenever.Time(12, 0))
+        .assume_tz(con["timezone"] if "timezone" in con else "Utc")
+    )
+    return now < end_date.add(days=7)
+
+
+json.dump(
+    sorted(
+        [con for con in cons if pred(con)],
+        key=lambda con: (whenever.Date.parse_common_iso(con["end_date"]), con["id"]),
+    ),
+    sys.stdout,
+)

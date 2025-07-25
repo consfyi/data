ffede2232f44a131eba9c0b90248d049b41eee2d
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-24 21:08:04 -0700

    add gc phase

--- .tools/digest.py
+++ .tools/digest.py
@@ -8,13 +8,16 @@
 # ///
 
 import json
+import pathlib
 import os
-import sys
 import toml
 import tzfpy
 import whenever
 
+OUTPUT_DIR = pathlib.Path(os.environ["OUTPUT_DIR"])
+
 cons = []
+
 
 for fn in os.listdir("."):
     id, ext = os.path.splitext(fn)
@@ -40,15 +43,25 @@ def pred(con):
     return now < end_date.add(days=7) and not con.get("canceled", False)
 
 
-json.dump(
-    sorted(
-        [con for con in cons if pred(con)],
-        key=lambda con: (
-            whenever.Date.parse_common_iso(con["startDate"]),
-            whenever.Date.parse_common_iso(con["endDate"]),
-            con["id"],
-        ),
-    ),
-    sys.stdout,
-    ensure_ascii=False,
+active = []
+for con in cons:
+    if pred(con):
+        active.append(con)
+    else:
+        id = con["id"]
+        os.rename(f"{id}.toml", f"archived/{id}.toml")
+
+active.sort(
+    key=lambda con: (
+        whenever.Date.parse_common_iso(con["startDate"]),
+        whenever.Date.parse_common_iso(con["endDate"]),
+        con["id"],
+    )
 )
+
+with open(OUTPUT_DIR / "active.json", "w") as f:
+    json.dump(
+        active,
+        f,
+        ensure_ascii=False,
+    )

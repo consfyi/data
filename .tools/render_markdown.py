ac70c9609e6635be53f90225995fd58740b30377
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-27 01:04:11 -0700

    add syntax highlighting

--- .tools/render_markdown.py
+++ .tools/render_markdown.py
@@ -2,14 +2,18 @@
 # /// script
 # dependencies = [
 #   "mistune",
+#   "pygments",
 # ]
 # ///
 
 import mistune
 import sys
+from pygments import highlight
+from pygments.lexers import get_lexer_by_name
+from pygments.formatters import html
 
 
-class TitleExtractingRenderer(mistune.HTMLRenderer):
+class MyRenderer(mistune.HTMLRenderer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.title = None
@@ -19,8 +23,15 @@ class TitleExtractingRenderer(mistune.HTMLRenderer):
             self.title = text
         return super().heading(text, level)
 
+    def block_code(self, code, info=None):
+        if info is not None:
+            lexer = get_lexer_by_name(info, stripall=True)
+            formatter = html.HtmlFormatter(cssclass="codehilite")
+            return highlight(code, lexer, formatter)
+        return "<pre><code>" + mistune.escape(code) + "</code></pre>"
 
-renderer = TitleExtractingRenderer()
+
+renderer = MyRenderer()
 markdown = mistune.create_markdown(renderer=renderer)
 html = markdown(sys.stdin.read())
 
@@ -32,6 +43,7 @@ print(
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@lowlighter/matcha@3.0.0/dist/matcha.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pygments-css@1.0.0/friendly.css">
     <title>{renderer.title}</title>
 </head>
 <body>

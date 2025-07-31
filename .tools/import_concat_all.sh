5f70c20d83bf4893dd4cd2881b5ae38a0465aee8
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-30 20:32:39 -0700

    AnthroExpo: concat import

--- .tools/import_concat_all.sh
+++ .tools/import_concat_all.sh
@@ -8,3 +8,4 @@ $concat_importer megaplex.json https://reg.megaplexcon.org
 $concat_importer further-confusion.json https://reg.furtherconfusion.org
 $concat_importer biggest-little-fur-con.json https://reg.goblfc.org
 $concat_importer pawcon.json https://reg.pacanthro.org
+$concat_importer anthroexpo.json https://reg.anthroexpo.net

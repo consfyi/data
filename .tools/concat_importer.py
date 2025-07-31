4bc7573042c7b82cd0e62a8be8b7b987f62e2700
tolf <140963545+tolfdog@users.noreply.github.com>
2025-07-30 20:53:10 -0700

    add regfox import i guess, and remove sourcing for anthrocon because it was imported from concat

--- .tools/concat_importer.py
+++ .tools/concat_importer.py
@@ -18,83 +18,90 @@ import whenever
 
 logging.basicConfig(level=logging.INFO)
 
-GOOGLE_MAPS_API_KEY = os.environ["GOOGLE_MAPS_API_KEY"]
-gmaps = googlemaps.Client(key=GOOGLE_MAPS_API_KEY)
 
-_, fn, concat_url = sys.argv
+def main():
+    gmaps = googlemaps.Client(key=os.environ["GOOGLE_MAPS_API_KEY"])
 
-series_id, ext = os.path.splitext(fn)
+    _, fn, concat_url = sys.argv
 
-with open(fn) as f:
-    series = json.load(f)
+    series_id, ext = os.path.splitext(fn)
 
-resp = httpx.get(f"{concat_url}/api/config")
-resp.raise_for_status()
-config = resp.json()
-for convention in config["conventions"]:
-    start_date = whenever.OffsetDateTime.parse_common_iso(convention["startAt"]).date()
-    end_date = whenever.OffsetDateTime.parse_common_iso(convention["endAt"]).date()
+    with open(fn) as f:
+        series = json.load(f)
 
-    id = f"{series_id}-{start_date.year}"
+    resp = httpx.get(f"{concat_url}/api/config")
+    resp.raise_for_status()
+    config = resp.json()
+    for convention in config["conventions"]:
+        start_date = whenever.OffsetDateTime.parse_common_iso(
+            convention["startAt"]
+        ).date()
+        end_date = whenever.OffsetDateTime.parse_common_iso(convention["endAt"]).date()
 
-    for i, e in enumerate(series["events"]):
-        if whenever.Date.parse_common_iso(e["startDate"]) <= start_date:
-            break
-    else:
-        i = len(series["events"])
+        id = f"{series_id}-{start_date.year}"
 
-    if i < len(series["events"]):
-        previous_event = series["events"][i]
-        if previous_event["id"] == id:
-            continue
+        for i, e in enumerate(series["events"]):
+            if whenever.Date.parse_common_iso(e["startDate"]) <= start_date:
+                break
+        else:
+            i = len(series["events"])
 
-    venue = convention["venue"]
-    country = config["organization"]["country"]
+        previous_event = None
+        if i < len(series["events"]):
+            previous_event = series["events"][i]
+            if previous_event["id"] == id:
+                continue
 
-    session_token = str(uuid.uuid4())
-    predictions = gmaps.places_autocomplete(
-        f"{venue}, {country}", session_token=session_token
-    )
+        venue = convention["venue"]
+        country = config["organization"]["country"]
 
-    venue = convention["venue"]
-    address = None
-
-    if len(predictions) == 0:
-        lat_lng = None
-    else:
-        prediction, *_ = predictions
-        st = prediction["structured_formatting"]
-        if "secondary_text" in st:
-            address = st["secondary_text"]
-
-        place = gmaps.place(
-            prediction["place_id"],
-            session_token=session_token,
-            fields=["geometry/location"],
+        session_token = str(uuid.uuid4())
+        predictions = gmaps.places_autocomplete(
+            f"{venue}, {country}", session_token=session_token
         )
-        l = place["result"]["geometry"]["location"]
-        lat_lng = [l["lat"], l["lng"]]
 
-    url = concat_url
-    if series["events"]:
-        url = series["events"][0]["url"]
-    elif url.startswith("https://reg."):
-        url = f"https://{url.removeprefix('https://reg.')}"
+        venue = convention["venue"]
+        address = None
 
-    event = {
-        "id": id,
-        "name": f"{series['name']} {start_date.year}",
-        "url": url,
-        "startDate": start_date.format_common_iso(),
-        "endDate": end_date.format_common_iso(),
-        "venue": venue,
-        "address": address,
-        "country": country,
-        "latLng": lat_lng,
-    }
-    logging.info(f"imported: {event}")
-    series["events"].insert(i, {k: v for k, v in event.items() if v is not None})
+        if len(predictions) == 0:
+            lat_lng = None
+        else:
+            prediction, *_ = predictions
+            st = prediction["structured_formatting"]
+            if "secondary_text" in st:
+                address = st["secondary_text"]
 
-with open(fn, "w") as f:
-    json.dump(series, f, indent=2, ensure_ascii=False)
-    f.write("\n")
+            place = gmaps.place(
+                prediction["place_id"],
+                session_token=session_token,
+                fields=["geometry/location"],
+            )
+            l = place["result"]["geometry"]["location"]
+            lat_lng = [l["lat"], l["lng"]]
+
+        url = concat_url
+        if previous_event is not None:
+            url = previous_event["url"]
+        elif url.startswith("https://reg."):
+            url = f"https://{url.removeprefix('https://reg.')}"
+
+        event = {
+            "id": id,
+            "name": f"{series['name']} {start_date.year}",
+            "url": url,
+            "startDate": start_date.format_common_iso(),
+            "endDate": end_date.format_common_iso(),
+            "venue": venue,
+            "address": address,
+            "country": country,
+            "latLng": lat_lng,
+        }
+        logging.info(f"imported: {event}")
+        series["events"].insert(i, {k: v for k, v in event.items() if v is not None})
+
+    with open(fn, "w") as f:
+        json.dump(series, f, indent=2, ensure_ascii=False)
+        f.write("\n")
+
+
+main()
